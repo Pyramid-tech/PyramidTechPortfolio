2c4b84ebfc4d569c76741e67a59242b31cff7b5c
AliiAssi <ali@kamkalima.com>
2026-06-24 17:42:14 +0300

    Fix build: remove invalid 'smooth' option from useScroll

--- src/components/ui/section-opacity.tsx
+++ src/components/ui/section-opacity.tsx
@@ -16,7 +16,6 @@ const SectionOpacity: FC<Props> = ({ children, classes, offset }) => {
   const { scrollYProgress } = useScroll({
     target: container,
     offset: offset || ['end 0.9', 'start 0.9'],
-    smooth: 0,
   });
 
   const opacity = useTransform(scrollYProgress, [0, 1], [1, 0]);
