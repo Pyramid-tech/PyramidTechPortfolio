09079d9c260a38b101d634c9be194b44531762cb
AliiAssi <ali@kamkalima.com>
2026-08-05 21:44:57 +0300

    enhacements

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  async redirects() {
-    return [
-      { source: '/projects', destination: '/work', permanent: true },
-      { source: '/projects/:slug', destination: '/work/:slug', permanent: true },
-    ];
-  },
+  output: 'standalone'
 };
 
 export default nextConfig;
