31b825a15f51a497a5a020a0a0433822e110d231
AliiAssi <ali@kamkalima.com>
2026-08-07 17:03:26 +0300

    fix the book path

--- src/app/(marketing)/book/page.tsx
+++ src/app/(marketing)/book/page.tsx
@@ -1,5 +1,7 @@
 import { BookForm } from '@/components/forms';
 
+export const dynamic = 'force-dynamic';
+
 const Index = () => {
   return (
     <section className="min-h-screen w-full bg-bg-1 px-4 pb-12 pt-24 sm:pb-16 sm:pt-28">
